270a206fad86568df2cb097d9431d11536aed52f
liatalony <liatalony311@gmail.com>
2022-05-31 13:02:17 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+const role_model = require('./role_model');
+
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+
+
+app.get('/', (req, res) => {
+    role_model.getRoles('customer')
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    })
+  })
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
