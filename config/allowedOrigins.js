73810d0526a7153d1e49da63697370614da67975
graeme <grae0026@stud.kea.dk>
2022-06-14 21:20:07 +0200

    test cors options

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
-    '127.0.0.1:3000', 
-    'http://localhost:3000', 
-    'http://localhost:3001'
+	"127.0.0.1:3000",
+	"http://localhost:3000",
+	"http://localhost:3001",
+	"http://172.104.238.112:3001",
 ];
 
 module.exports = allowedOrigins;
