5221e68a5eb624a813c008b97a41fdcb736d33ab
graeme <grae0026@stud.kea.dk>
2022-06-16 16:46:27 +0200

    check static path

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ const ROLES_LIST = require("./config/ROLES_LIST");
 const allowedRole = require("./middleware/verifyRoles");
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "../frontend/")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", function (req, res) {
-	res.sendFile(path.join(__dirname, "../frontend/", "index.html"));
+	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.use(credentials);
