9059172bb21069536551065bf03f1fb6b629856b
liatalony <liatalony311@gmail.com>
2022-06-19 13:30:38 +0200

    change limit of home page

--- controllers/getNewProductsController.js
+++ controllers/getNewProductsController.js
@@ -2,7 +2,7 @@ const pool = require('../config/DBConn');
 
 const getNewProducts = async (req, res) => {
       try {
-         const allNewProducts = await pool.query('SELECT p.product_id, p.product_headline, p.product_price, img.image_name FROM product p JOIN image img ON img.product_id = p.product_id WHERE img.is_main = true AND status=$1 AND is_reserved=false ORDER BY p.date_inserted DESC LIMIT 10', ['active'])
+         const allNewProducts = await pool.query('SELECT p.product_id, p.product_headline, p.product_price, img.image_name FROM product p JOIN image img ON img.product_id = p.product_id WHERE img.is_main = true AND status=$1 AND is_reserved=false ORDER BY p.date_inserted DESC LIMIT 12', ['active'])
 
         if (allNewProducts.rows.length == 0) {
           return res.status(204).send('Something')
